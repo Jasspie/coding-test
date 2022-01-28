567b6fab9c238feff58df4a63641c93364b1fffc
Jason Li <lijason02@hotmail.com>
2022-01-28 11:45:27 -0500

    created carousel

---
+++ client/src/components/Carousel.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { useImage } from "../context/ImageContext";
+import { Spinner } from "react-bootstrap";
+import "./Carousel.css";
+
+export const Carousel = () => {
+  const { images, index, getActive, loading } = useImage();
+  const active = getActive();
+  var title;
+  if (active === "all") title = "Cats 🐱 & Sharks 🦈";
+  else if (active === "cat") title = "Cats 🐱";
+  else if (active === "shark") title = "Sharks 🦈";
+  else title = "Select Animals";
+
+  console.log(loading);
+  return (
+    <div className="image-container">
+      <h1 className="title">{title}</h1>
+      {loading ? (
+        <div style={{ paddingTop: "5vh" }}>
+          <Spinner
+            animation="border"
+            size="lg"
+            style={{ height: "10rem", width: "10rem" }}
+          />
+        </div>
+      ) : (
+        images.length !== 0 && (
+          <img src={images[index]} alt={active} style={{ height: "40vh" }} />
+        )
+      )}
+    </div>
+  );
+};
